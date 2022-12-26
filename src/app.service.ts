3f7a0583ab39ea4305cff61f956fcc7f0c58351b
Kevin <juwon8891@gmail.com>
2022-12-26 21:51:09 +0900

    🎨 ch1 done.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { UsersService } from './users/users.service';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly configService: ConfigService) {}
-  getHello() {
-    return this.configService.get('SECRET');
+  constructor(private usersService: UsersService) {}
+
+  async getHello() {
+    this.usersService.getUser();
+    this.getWow();
+    return process.env.SECRET; // better: this.configService.get('SECRET');
   }
+
+  async getWow() {}
 }
